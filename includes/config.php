fa823f11c444d72330306b118ed1bfd7ece734f0
KsyTim <timoshenko.ksenia.1998@gmail.com>
2021-11-12 21:06:19 +0400

    added config file with const

--- includes/config.php
+++ includes/config.php
@@ -3,5 +3,11 @@
 define('SITE_NAME', 'Twitter');
 // адрес сайта
 define('HOST', 'http://' . $_SERVER['HTTP_HOST']);
-// 
+// настройки для БД
 define('DB_HOST', 'localhost');
+// название базы данных 
+define('DB_NAME', 'twitter');
+// пользователь, подключающийся к БД
+define('DB_USER', 'root');
+// пароль для подключения к БД
+define('DB_PASS', 'root');
